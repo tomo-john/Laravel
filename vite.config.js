68e139589235f09cc95f786a48d752a2a525f0cf
tomo-john <keririnkery.kery@gmail.com>
2025-12-27 13:59:08 +0900

    feat: Laravel Breeze

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-              'resources/css/app.css',
-              'resources/js/app.js',
-              'resources/js/dog.js',
-              'resources/js/tailwind.js',
-              'resources/js/test.js',
-              'resources/js/animation.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
